20595b63408b85659e472fce40a49e74d97b3d63
Tzingtao Chow <Cyphexl@outlook.com>
2020-04-02 23:57:40 +0100

    finish

--- gen-chart.js
+++ gen-chart.js
@@ -1,4 +1,30 @@
-const genChart = (data, container, scale) => {
+const genChart = config => {
+  const options = {
+    data: null,
+    container: null,
+    scale: null,
+    zoomable: false,
+    brushCallback: null,
+    themeColor: "#ff4553"
+  };
+
+  const min = d3.min(config.data, d => d.value);
+  const max = d3.max(config.data, d => d.value);
+  options.scale = [min, max];
+
+  for (let option in config) {
+    options[option] = config[option];
+  }
+  const {
+    data,
+    container,
+    scale,
+    zoomable,
+    brushCallback,
+    themeColor
+  } = options;
+
+  $(container).html("");
   let yAxis = g =>
     g
       .attr("transform", `translate(${margin.left},0)`)
@@ -24,8 +50,7 @@ const genChart = (data, container, scale) => {
 
   let y = d3
     .scaleLinear()
-    .domain(scale || [0, d3.max(data, d => d.value)])
-    .nice()
+    .domain(scale)
     .range([height - margin.bottom, margin.top]);
 
   let x = d3
@@ -36,20 +61,47 @@ const genChart = (data, container, scale) => {
 
   const svg = d3.create("svg").attr("viewBox", [0, 0, width, height]);
 
-  svg
+  const rects = svg
     .append("g")
-    .attr("fill", "steelblue")
+    .attr("fill", "#555")
     .selectAll("rect")
     .data(data)
     .join("rect")
     .attr("x", (d, i) => x(i))
     .attr("y", d => y(d.value))
-    .attr("height", d => y(0) - y(d.value))
+    .attr("height", d => {
+      let h = y(scale[0]) - y(d.value);
+      return h > 0 ? h : 0;
+    })
     .attr("width", x.bandwidth());
 
   svg.append("g").call(xAxis);
-
   svg.append("g").call(yAxis);
+
+  if (!zoomable) {
+    svg
+      .append("rect")
+      .attr("class", "blend-mode-mask")
+      .attr("width", width)
+      .attr("height", height)
+      .style("mix-blend-mode", "lighten");
+  }
+
+  if (zoomable) {
+    const brush = d3.brush().on("start brush end", brushed);
+    svg.call(brush);
+    function brushed() {
+      const selection = d3.event.selection;
+      if (selection) {
+        brushCallback(selection, x, y);
+      }
+      d3.select(".selection")
+        .attr("fill-opacity", 1)
+        .attr("stroke-width", "10")
+        .style("mix-blend-mode", "lighten")
+        .attr("stroke", "rgba(255,255,255,1)");
+    }
+  }
 
   $(container).append(svg.node());
 };
